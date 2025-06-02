baef1812adec0865bf4875d290e8d25bbdbfa44c
Sagar Kale <46244365+sagarkale94@users.noreply.github.com>
2025-06-02 19:43:05 +0530

    Delete responseentityexceptionhandler directory

--- responseentityexceptionhandler/src/main/java/co/in/sagarkale/responseentityexceptionhandler/exceptions/UserInActiveException.java
+++
@@ -1,7 +0,0 @@
-package co.in.sagarkale.responseentityexceptionhandler.exceptions;
-
-public class UserInActiveException extends RuntimeException{
-    public UserInActiveException(String message) {
-        super(message);
-    }
-}
--- responseentityexceptionhandler/src/main/java/co/in/sagarkale/responseentityexceptionhandler/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package co.in.sagarkale.responseentityexceptionhandler.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
